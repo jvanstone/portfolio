aec29e053f5d3ca69d504d62b4cc07d40c48273a
Jason Vanstone <jason@vanstoneonline.com>
2024-07-19 12:35:20 +0200

    Added webpack to gulp file

--- wp-content/themes/portfolio/gulpfile.js
+++ wp-content/themes/portfolio/gulpfile.js
@@ -1,49 +1,73 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const minifyCss = require('gulp-clean-css');
-//const sourceMap = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const uglify = require('gulp-terser');
 const gulpESLintNew = require('gulp-eslint-new');
 const sass = require('gulp-sass')(require('sass'));
+const webpack = require('webpack');
+const gulpWebpack = require('webpack-stream');
+const path = require('path');
 
-const styles = () => {
-	return src('./resources/scss/style.scss')
-    	.pipe(sass().on('error', sass.logError))
-		.pipe(minifyCss())
-		.pipe(concat('style.css'))
-		.pipe(dest('./dist/css/'));
+// Paths constants
+const paths = {
+    js: {
+        src: './resources/js/common.js',
+        dest: './dist/js/'
+    },
+    scss: {
+        src: './resources/scss/style.scss'
+    },
+    html: {
+        src: './**/*.html'
+    }
 };
 
+// Function to compile Sass into CSS, minify it, concatenate, and output to dist/css
+function styles() {
+    return src(paths.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCss())
+        .pipe(concat('style.css'))
+        .pipe(dest('./dist/css/'));
+}
 
-const jsWatchFiles = (cb) => {
-	src('./resources/js/common.js').pipe(dest('./dist/js/'));
-	cb();
-};
+// Function to bundle JS using Webpack and output to dist/js
+function bundleJS(env) {
+    return src(paths.js.src)
+		.pipe(gulpWebpack(require('./webpack.config.js')({ NODE_ENV: env }), webpack))
+        .pipe(dest(paths.js.dest));
+}
 
-const jsFile = (cb) => {
-	src('./resources/js/common.js').pipe(uglify()).pipe(dest('./dist/js/'));
-	cb();
-};
-
-const runLinter = () => {
-	return src(['scripts/*.js']) // Read files.
-		.pipe(gulpESLintNew({ fix: true })) // Lint files, create fixes.
-		.pipe(gulpESLintNew.fix()) // Fix files if necessary.
-		.pipe(gulpESLintNew.format()) // Output lint results to the console.
-		.pipe(gulpESLintNew.failAfterError()); // Exit with an error if problems are found.
-};
+//Set Webpack Enviroment names.
+const webpackDevelopment = () => bundleJS('development');
+const webpackProduction = () => bundleJS('production');
 
 
-const watchFiles = () => {
-	watch('./resources/scss/**/*.scss', styles);
-	watch(
-		['./resources/js/**.js', '!node_modules/**'],
-		parallel(runLinter, styles, jsWatchFiles)
-	  );	
-};	
+// Function to minify JS files
+function jsFile() {
+    return src(paths.js.src)
+        .pipe(uglify())
+        .pipe(dest(paths.js.dest));
+}
 
+// Function to run ESLint
+function runLinter() {
+    return src(['./resources/js/**/*.js', '!node_modules/**'])
+        .pipe(gulpESLintNew({ fix: true }))
+        .pipe(gulpESLintNew.format())
+        .pipe(gulpESLintNew.failAfterError());
+}
+
+// Function to watch files for changes
+function watchFiles() {
+    watch('./resources/scss/**/*.scss', styles);
+    watch(['./resources/js/**/*.js', '!node_modules/**'], parallel(runLinter, jsFile, webpackDevelopment));
+}
+
+// Define Gulp tasks
 exports.styles = styles;
 exports.jsFile = jsFile;
+exports.bundleJS = webpackProduction;
 exports.lint = runLinter;
-exports.build = series(runLinter, styles, jsFile);
+exports.build = series(runLinter, styles, jsFile, webpackProduction);
 exports.default = watchFiles;
--- wp-content/themes/portfolio/webpack.config.js
+++ wp-content/themes/portfolio/webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-	entry: './resources/js/common.js',
-	resolve: {
-		extensions: ['.js'],
-		modules: [path.resolve(__dirname, 'src'), 'node_modules']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist/js'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-	},
-	plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        })
-    ],
-    mode: 'production'
+module.exports = (env) => {
+	const isDevelopment = env.NODE_ENV === 'development';
+
+	return {
+		mode: isDevelopment ? 'development' : 'production',
+		entry: './resources/js/common.js',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, './dist/js'),
+		},
+		devtool: isDevelopment ? 'source-map' : false,
+		resolve: {
+			extensions: ['.js'],
+            modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'] // Ensure node_modules are resolved
+        },
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: 'babel-loader'
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				}
+			]
+		},
+		plugins: [
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery'
+			})
+		],
+	};
 };
