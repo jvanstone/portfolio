046f9d9aa6bd67b94aa36980a28f0ec514c2346f
Jason Vanstone <jason@vanstoneonline.com>
2026-02-28 04:08:51 -0500

    remove cf7-anitspam completly

--- wp-content/plugins/_cf7-antispam/build/admin-scripts.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '93b77da6192d983e5273');
--- wp-content/plugins/_cf7-antispam/build/script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c50d8d93495e825c14d6');
